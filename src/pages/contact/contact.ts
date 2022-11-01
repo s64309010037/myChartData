b76cf0d0196b94651109acdbcfc8895086c5ca9d
s64309010037 <s64309010037@ptnicec.ac.th>
2022-11-01 17:49:56 +0700

    first commit

---
+++ src/pages/contact/contact.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html'
+})
+export class ContactPage {
+
+  // Doughnut
+  public doughnutChartLabels:string[] = ['ปวช.1', 'ปวช.2', 'ปวช.3','ปวส.1','ปวส.2'];
+  public doughnutChartData:number[] = [10,7,6,32,37,];
+  public doughnutChartType:string = 'doughnut';
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
